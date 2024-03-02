bd8220259dfa588d5932015d6d6321e84f8291d1
radikru96 <radikru96@gmail.com>
2024-03-02 17:49:56 +0300

    TranslucentBackground

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MyWidget wgt;
+    wgt.setAttribute(Qt::WA_TranslucentBackground);
     wgt.show();
     wgt.resize(201, 121);
     return a.exec();
