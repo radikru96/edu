9f359915c5455c1415c1560419b88cc708216e12
radikru96 <radikru96@gmail.com>
2024-03-02 17:25:33 +0300

    set gradient

--- src/MyWidget.cpp
+++ src/MyWidget.cpp
@@ -6,6 +6,11 @@ void MyWidget::paintEvent(QPaintEvent *)
 {
     QPainter painter(this);
     QRect r(0, 0, 200, 120);
+    QLinearGradient gra(80, 0, 120, 120);
+    gra.setColorAt(0, Qt::red);
+    gra.setColorAt(0.5, Qt::green);
+    gra.setColorAt(1, Qt::blue);
+    painter.setPen(QPen(gra, 0));
     painter.setFont(QFont("Times", 25, QFont::Normal));
     painter.drawRect(r);
     painter.drawText(r, Qt::AlignCenter | Qt::TextWordWrap, "DrawText" );
