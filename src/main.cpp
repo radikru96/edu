9d7f41dc4b5a892b401736c51804a2134fd7e7d0
radikru96 <radikru96@gmail.com>
2024-03-03 19:27:54 +0300

    list 21.1

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,19 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QWidget wgt;
-    wgt.show();
+    QGraphicsScene scene(QRectF(-100, -100, 300, 300));
+    QGraphicsView view(&scene );
+    QGraphicsRectItem *pRectItem = scene.addRect(QRectF( -30, -30, 120, 80),
+                                                 QPen(Qt::black),
+                                                 QBrush(Qt::green));
+    pRectItem->setFlags(QGraphicsItem::ItemIsMovable);
+    QGraphicsTextItem *pTextItem = scene.addText("Move us with your mouse");
+    pTextItem->setFlags(QGraphicsItem::ItemIsMovable);
+    QGraphicsLineItem *pLineItem = scene.addLine(QLineF(-10, -10, -80, -80), QPen(Qt::red, 2));
+    pLineItem->setFlags(QGraphicsItem::ItemIsMovable);
+    QGraphicsPixmapItem *pPixmapItem = scene.addPixmap(QPixmap(":/images/Windows.png"));
+    pPixmapItem->setFlags(QGraphicsItem::ItemIsMovable);
+
+    view.show();
     return a.exec();
 }
