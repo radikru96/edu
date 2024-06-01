4067ae88b5419f7e7405288fb5b6caa903062904
radikru96 <radikru96@gmail.com>
2024-06-01 23:16:19 +0300

    release

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,19 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QWidget wgt;
-    wgt.show();
+    QLabel lbl;
+    lbl.setPixmap(QPixmap(":/images/MacOS.png"));
+    QGraphicsColorizeEffect effect;
+    lbl.setGraphicsEffect(&effect);
+    QPropertyAnimation anim(&effect, "color");
+    anim.setStartValue(QColor(Qt::gray));
+    anim.setKeyValueAt(0.25f, QColor(Qt::green));
+    anim.setKeyValueAt(0.5f, QColor(Qt::blue));
+    anim.setKeyValueAt(0.75f, QColor(Qt::red));
+    anim.setEndValue(QColor(Qt::black));
+    anim.setDuration(3000);
+    anim.setLoopCount(-1);
+    anim.start();
+    lbl.show();
     return a.exec();
 }
