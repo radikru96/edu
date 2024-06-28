cb10173f50cb6dce1cf527e935c4ef3de130e9ae
radikru96 <radikru96@gmail.com>
2024-06-28 21:40:48 +0300

    list 45.1-45.3 test's PASS

--- src/MyClass.h
+++ src/MyClass.h
@@ -3,8 +3,8 @@
 
 class MyClass {
 public:
-    int min( int n1, int n2 ) { return n1 > n2 ? n1 : n2; }
-    int max( int n1, int n2 ) { return n1 < n2 ? n1 : n2; }
+    int min( int n1, int n2 ) { return n1 < n2 ? n1 : n2; }
+    int max( int n1, int n2 ) { return n1 > n2 ? n1 : n2; }
 };
 
 #endif // MYCLASS_H
