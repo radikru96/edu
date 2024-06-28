b3ac53863ce1dc918ba984d0d962c49e5eec1b6a
radikru96 <radikru96@gmail.com>
2024-06-28 22:48:28 +0300

    add *_data() 'list 45.4-45.8'

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,50 @@
 class Test_MyClass : public QObject {
     Q_OBJECT
 private slots:
+    void min_data();
+    void max_data();
     void min();
     void max();
 };
 
+void Test_MyClass::min_data()
+{
+    QTest::addColumn<int>("arg1");
+    QTest::addColumn<int>("arg2");
+    QTest::addColumn<int>("result");
+    QTest::newRow("min_test1") << 25 << 0 << 0;
+    QTest::newRow("min_test2") << -12 << -5 << -12;
+    QTest::newRow("min_test3") << 2007 << 2007 << 2007;
+    QTest::newRow("min_test4") << -12 << 5 << -12;
+}
+
+void Test_MyClass::max_data()
+{
+    QTest::addColumn<int>("arg1");
+    QTest::addColumn<int>("arg2");
+    QTest::addColumn<int>("result");
+    QTest::newRow("max_test1") << 25 << 0 << 25;
+    QTest::newRow("max_test2") << -12 << -5 << -5;
+    QTest::newRow("max_test3") << 2007 << 2007 << 2007;
+    QTest::newRow("max_test4") << -12 << 5 << 5;
+}
+
 void Test_MyClass::min()
 {
     MyClass myClass;
-    QCOMPARE(myClass.min( 25, 0), 0);
-    QCOMPARE(myClass.min( -12, -5), -12);
-    QCOMPARE(myClass.min( 2007, 2007), 2007);
-    QCOMPARE(myClass.min( -12, 5), -12);
+    QFETCH(int, arg1);
+    QFETCH(int, arg2);
+    QFETCH(int, result);
+    QCOMPARE(myClass.min( arg1, arg2), result);
 }
 
 void Test_MyClass::max()
 {
     MyClass myClass;
-    QCOMPARE(myClass.max( 25, 0), 25);
-    QCOMPARE(myClass.max( -12, -5), -5);
-    QCOMPARE(myClass.max( 2007, 2007), 2007);
-    QCOMPARE(myClass.max( -12, 5), 5);
+    QFETCH(int, arg1);
+    QFETCH(int, arg2);
+    QFETCH(int, result);
+    QCOMPARE(myClass.max( arg1, arg2), result);
 }
 
 QTEST_MAIN(Test_MyClass)
