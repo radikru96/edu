c295716070e3917e5ab35fc92589ede8a259e144
radikru96 <radikru96@gmail.com>
2024-06-29 21:27:08 +0300

    list 49.5

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
-#include <QtCore>
 #include <QJSEngine>
+#include <QtWidgets>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QApplication a(argc, argv);
+    QLabel *plbl = new QLabel;
     QJSEngine scriptEngine;
-    QJSValue value = scriptEngine.evaluate( "2 * 8" );
-    qDebug() << value.toInt();
-    return 0;
+    QJSValue scriptLbl = scriptEngine.newQObject(plbl);
+    scriptEngine.globalObject().setProperty( "lbl", scriptLbl );
+    scriptEngine.evaluate( "lbl.text = 'Hello, JavaCreep!'" );
+    scriptEngine.evaluate( "lbl.show()" );
+    return a.exec();
 }
