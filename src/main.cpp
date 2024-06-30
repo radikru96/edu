6c60dee63bda34d0d6bd882e55f02e6df4c2bdf4
radikru96 <radikru96@gmail.com>
2024-07-01 00:03:39 +0300

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,10 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QLabel *plbl = new QLabel;
     QJSEngine scriptEngine;
-    QJSValue scriptLbl = scriptEngine.newQObject(plbl);
-    scriptEngine.globalObject().setProperty( "lbl", scriptLbl );
+
+    QJSValue scriptLbl = scriptEngine.newQObject(plbl);             //QLabel's object insert to JSEngine
+    scriptEngine.globalObject().setProperty( "lbl", scriptLbl );    //Object's declaration for JSEngine
+
     scriptEngine.evaluate( "lbl.text = 'Hello, JavaCreep!'" );
     scriptEngine.evaluate( "lbl.show()" );
     return a.exec();
