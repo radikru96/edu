bf69a2607751cbe096434f500f87a3e366e3ca66
radikru96 <radikru96@gmail.com>
2024-07-21 20:38:23 +0300

    list 60.4-60.6

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
-#include <QApplication>
+#include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlComponent>
+#include "myconnections.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
-    engine.load( QUrl( "qrc:/src/main.qml" ) );
+    QQmlComponent component( &engine, QUrl("qrc:/src/main.qml") );
+    MyConnections mc;
+    QObject *pobj = component.create();
+    QObject *pcmdQuitButton = pobj->findChild<QObject *>( "QuitButton" );
+    if ( pcmdQuitButton )
+        QObject::connect( pcmdQuitButton, SIGNAL(quitClicked()), &mc, SLOT(slotQuit()) );
+    QObject *pcmdInfoButton = pobj->findChild<QObject *>( "InfoButton" );
+    if ( pcmdInfoButton )
+        QObject::connect( pcmdInfoButton, SIGNAL(infoClicked()), &mc, SLOT(slotInfo(QString)) );
     return app.exec();
 }
