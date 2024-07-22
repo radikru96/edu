bf3a8f14cdcacd9b047dd38552490f4a5df06d9e
radikru96 <radikru96@gmail.com>
2024-07-22 21:02:28 +0300

    fixed to list 60.9

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include <QApplication>
+#include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "calculation.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    QGuiApplication app(argc, argv);
     qmlRegisterType<Calculation>( "com.myinc.Calculation", 1, 0, "Calculation" );
     QQmlApplicationEngine engine;
     engine.load( QUrl( "qrc:/src/main.qml" ) );
