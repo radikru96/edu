fcc0dd29b52cdd51352b2ec35b19a2c210731bca
radikru96 <radikru96@gmail.com>
2024-08-02 21:57:15 +0300

    list 60.20-60.22

--- src/imageprovider.cpp
+++ src/imageprovider.cpp
@@ -1,28 +1,33 @@
 #include "imageprovider.h"
-#include <QPainter>
 
-ImageProvider
-    ::ImageProvider
-    (QQuickItem *parent)
-    : QQuickPaintedItem{parent}, m_color(Qt::black) {}
-
-void ImageProvider
-    ::paint(QPainter *ppainter)
-{
-    ppainter->setRenderHint( QPainter::Antialiasing, true );
-    ppainter->setBrush( QBrush( colorValue()) );
-    ppainter->setPen( Qt::NoPen );
-    ppainter->drawEllipse( boundingRect() );
+QImage ImageProvider::brightness(const QImage *imgOrig, int n) {
+    QImage imgTemp = imgOrig;
+    qint32 nHeight = imgTemp.height();
+    qint32 nWidth = imgTemp.width();
+    for ( qint32 y = 0; y < nHeight; ++y ) {
+        QRgb *tempLine = reinterpret_cast<QRgb*>( imgTemp.scanLine(y) );
+        for ( qint32 x = 0; x < nWidth; ++x ) {
+            int r = qRed( *tempLine) + n;
+            int g = qGreen( *tempLine) + n;
+            int b = qBlue( *tempLine) + n;
+            int a = qAlpha( *tempLine);
+            *tempLine++ = qRgba( r > 255 ? 255 : r < 0 ? 0 : r,
+                                 g > 255 ? 255 : g < 0 ? 0 : g,
+                                 b > 255 ? 255 : b < 0 ? 0 : b,
+                                 a );
+        }
+    }
+    return imgTemp;
 }
 
-QColor ImageProvider
-    ::colorValue() const
-{
-    return m_color;
-}
+ImageProvider::ImageProvider() : QQuickImageProvider( QQuickImageProvider::Image ) {}
 
-void ImageProvider
-    ::setColorValue(const QColor &col)
-{
-    m_color = col;
+QImage ImageProvider::requestImage(const QString &strId, QSize *ps, const QSize & /*requestedSize*/){
+    QStringList lst = strId.split(";");
+    bool bOk = false;
+    int nBrightness = lst.last().toInt(&bOk);
+    QImage img = brightness( QImage( "qrc:/" +, lst.first() ), nBrightness );
+    if (ps)
+        *ps = img.size();
+    return img;
 }
